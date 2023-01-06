750a9f37de93fc4659a135aac76944760a81e452
estherthankgod <tripcy18@gmail.com>
2023-01-06 06:46:58 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-if _name_ == "_main_":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if__name__=="__main__":
+"""Printsomeof1and2"""
+from add_0 import add
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
